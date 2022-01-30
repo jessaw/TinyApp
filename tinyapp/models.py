6d3afa3730cf550027b0c2a223179a4484ddff67
Jessica <jessica@Jessicas-MacBook-Air.local>
2022-01-29 23:29:14 -0500

    removed slug

--- tinyapp/models.py
+++ tinyapp/models.py
@@ -29,5 +29,5 @@ class Url(models.Model):
         return self.short_url
 
     def get_absolute_url(self):
-       return reverse('urls_detail', args=[str(self.pk)])
+       return reverse('urls-detail', args=[str(self.pk)])
 
